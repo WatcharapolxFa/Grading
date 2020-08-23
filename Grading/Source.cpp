2de5cfeab4d089a86a5d8ebc8931e73bb510d57c
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-23 11:16:26 +0700

    Success

--- Grading/Source.cpp
+++ Grading/Source.cpp
@@ -17,7 +17,38 @@ int main()
 			if ((scanf_s("%d", &num[2])))
 			{
 				num[3] = sum(num[0], num[1], num[2]);
-				
+				if (0 < num[3] && num[3] <= 49)
+				{
+					printf_s("F");
+				}
+				else if (50 <= num[3] && num[3] <= 54)
+				{
+					printf_s("D");
+				}
+				else if (55 <= num[3] && num[3] <= 59)
+				{
+					printf_s("D+");
+				}
+				else if (60 <= num[3] && num[3] <= 64)
+				{
+					printf_s("C");
+				}
+				else if (65 <= num[3] && num[3] <= 69)
+				{
+					printf_s("C+");
+				}
+				else if (70 <= num[3] && num[3] <= 74)
+				{
+					printf_s("B");
+				}
+				else if (75 <= num[3] && num[3] <= 79)
+				{
+					printf_s("B+");
+				}
+				else if (80 <= num[3] && num[3] <= 100)
+				{
+					printf_s("A");
+				}
 				
 			}
 			else
