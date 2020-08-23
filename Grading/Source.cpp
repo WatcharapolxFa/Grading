df61ae18c5d49be1a37539b31e729ad2b93acacc
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-23 11:13:00 +0700

    50 %

--- Grading/Source.cpp
+++ Grading/Source.cpp
@@ -1,9 +1,43 @@
 #include<stdio.h>
-
+int sum(int a , int b ,int c)
+{
+	int ans = 0;
+	ans = a + b + c;
+	return ans;
+}
 int main()
 {
+	int num[4];
+	if (scanf_s("%d", &num[0]))
+	{
+		if (scanf_s("%d", &num[1]))
+		{
 
 
+			if ((scanf_s("%d", &num[2])))
+			{
+				num[3] = sum(num[0], num[1], num[2]);
+				
+				
+			}
+			else
+			{
+				printf_s("Please In Put Number \n End Program");
+				
+			}
+
+		}
+		else
+		{
+			printf_s("Please In Put Number \n End Program ");
+		}
+
+
+	}
+	else
+	{
+		printf_s("Please In Put Number \n End Program ");
+	}
 
 
 
